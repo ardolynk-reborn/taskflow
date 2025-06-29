ae31a1bcf00ae05b46c2e73a734861d418b78b71
Michael Tikhonenko <ardolynk75@gmail.com>
2025-06-29 08:02:59 +0200

    Backend application created and integrated with Keycloak

---
+++ taskflow-be/src/main/java/com/ardolynk/taskflow/configuration/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.ardolynk.taskflow.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/public/**").permitAll()
+                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
---
+++ taskflow-be/src/test/java/com/ardolynk/taskflow/TaskflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardolynk.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
